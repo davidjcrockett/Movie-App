f946abc69f0b4b8b672c6ef108536a6ff6d20934
David J Crockett <crockett.davidj@gmail.com>
2022-11-21 16:51:28 -0800

    Updated API Key for .env file

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1cb8a2207e718162b37eb36283a1e3b4&page=1";
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const API_KEY = process.env.API_KEY;
 
 const mainEl = document.getElementById("#main");
 const formEl = document.getElementById("#form");

1da7a02095302eb638675bb1e80b39986c2992db
David J Crockett <crockett.davidj@gmail.com>
2022-11-23 21:26:39 -0800

    Layout styling in HTML

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const API_KEY = process.env.API_KEY;
 const BASE_URL = process.env.API_BASE_URL;
 
-const mainEl = document.getElementById("#main");
-const formEl = document.getElementById("#form");
-const searchEl = document.getElementById("#search");
+const moviesEl = document.getElementById("movies")
 
-export async function getPopularMovies(page = 1) {
+function App() {
+
+    export async function getPopularMovies(page = 1) {
     let data = []
     try {
         const response = await fetch(`${BASE_URL}movie/popular?api_key=${API_KEY}&page=${page}`)
@@ -16,3 +16,24 @@ export async function getPopularMovies(page = 1) {
     }
     return data
 }
+
+
+export async function renderMovies() {
+    const movies = await getPopularMovies()
+    console.log(movies)
+    moviesEl.innerHTML = movies?.map(movie => renderSingleMovie(movie)).join("")
+}
+
+function renderSingleMovie(movie) {
+    return (
+        `
+        <div class="col-4 col-lg-3 col-xl-2 p-1">
+            <img src="${config.image_base_url + movie?.poster_path}" class="img-fluid" >
+        </div>
+        `
+    )
+}
+
+}
+
+App();

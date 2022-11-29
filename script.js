db2bce1f1578086f2c4b3d140c64bf66ac9bd4bc
David J Crockett <crockett.davidj@gmail.com>
2022-11-29 15:16:27 -0800

    querySelectors for searching

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
 const API_KEY = process.env.API_KEY;
-const BASE_URL = process.env.API_BASE_URL;
+const image_base_url = `https://image.tmdb.org/t/p/w1280`
 
-const moviesEl = document.getElementById("movies")
-
-function App() {
-
-    export async function getPopularMovies(page = 1) {
-    let data = []
-    try {
-        const response = await fetch(`${BASE_URL}movie/popular?api_key=${API_KEY}&page=${page}`)
-        const responseData = await response.json()
-        data = responseData?.results
-    } catch (error) {
-        
-    }
-    return data
-}
-
-
-export async function renderMovies() {
-    const movies = await getPopularMovies()
-    console.log(movies)
-    moviesEl.innerHTML = movies?.map(movie => renderSingleMovie(movie)).join("")
-}
-
-function renderSingleMovie(movie) {
-    return (
-        `
-        <div class="col-4 col-lg-3 col-xl-2 p-1">
-            <img src="${config.image_base_url + movie?.poster_path}" class="img-fluid" >
-        </div>
-        `
-    )
-}
-
-}
-
-App();
+const input = document.querySelector('.search input');
+const btn = document.querySelector('.search button');
+const main_grid_title = document.querySelector('.favorites h1');
+const main_grid = document.querySelector('.favorites .movies-grid');
